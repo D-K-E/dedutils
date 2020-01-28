8941ff34bcf03d69e4743c78b92996be87a0c53f
Kaan Eraslan <kaaneraslan@gmail.com>
2020-01-28 03:31:41 +0100

    Entryfn added schemafn test added.
    Entryfn now implements fuzzy matching of entry/fields.
    It also implements adding new field values or entries.
    It needs tests now.
    
    Schemafn's makeSchema is tested and it passes the test.

--- src/entryfn.nim
+++ src/entryfn.nim
@@ -15,8 +15,8 @@ import termfn # getTermList
 ##     {"id": "entry-list-name-1",
 ##      "info": {
 ##          "field-name": [{
-##                              "field-name-id-1": "",
-##                              "probability": "",
+##                              "field-name-id-1": "term-id-46",
+##                              "probability": int,
 ##                              "value": ["term-id-1", "term-id-2"],
 ##                         },
 ##                         {
@@ -32,7 +32,6 @@ proc readSchema(): JsonNode =
     ## read schema.json for field term association values
     return parseFile(getSchemaPath())
 
-
 proc makeEntryTemplate(schema: JsonNode,
                        entrynb: int,
                        lstname: string): JsonNode =
@@ -44,3 +43,58 @@ proc makeEntryTemplate(schema: JsonNode,
         info[key] = newJArray()
     result["info"] = info
 
+proc getEntriesFromList*(lst: JsonNode,
+                         fuzzy: string): JsonNode {.raises: [ValueError].} =
+    ## get entries from entry list using fuzzy string
+    var entries = newJArray()
+    for entry in entries.items():
+        let entryId = entry["id"].getStr()
+        if fuzzy in entryId:
+            entries.add(entry)
+            continue
+        let entryInfo = entry["info"]
+        block inEntryBlock:
+            for fieldTpl in entryInfo.pairs():
+                let fname = fieldTpl.key.getStr()
+                if fuzzy in fname:
+                    entries.add(entry)
+                    break inEntryBlock
+                let farray = fieldTpl.val
+                for fieldObj in farray.items():
+                    let valarr = fieldObj["value"]
+                    for val in valarr.items():
+                        if fuzzy in val:
+                            entries.add(entry)
+                            break inEntryBlock
+    return entries
+
+proc getFieldByName(fieldName: string,
+                    entry: JsonNode): JsonNode {.raises: [ValueError].} =
+    ## get jarray associated to field from entry using its name
+    let einfo = entry["info"]
+    var found = false
+    for name, arr in einfo.pairs():
+        let nstr = name.getStr()
+        if nstr == fieldName:
+            found = true
+            return arr
+    if found == false:
+        var msg = "Field name " & fieldName & " does not exist in entry "
+        msg = msg & entry["id"].getStr()
+
+        raise newException(ValueError, msg)
+
+proc addValue2Field(fieldName: string,
+                    probability: uint8,
+                    fval: string,
+                    values: seq[string],
+                    entry: JsonNode): JsonNode =
+    "add value 2 field"
+    var fieldarr = getFieldByName(fieldName, entry)
+    let arrlen = len(fieldarr)
+    let valname = fieldName & "-" & $(arrlen)
+    let valobj = %* {valname: fval, "probability": probability,
+                     "value": values}
+    fieldarr.add(valobj)
+    entry["info"][fieldName] = fieldarr
+    return entry
