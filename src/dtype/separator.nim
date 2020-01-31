783a3cf306d246fae0252e8dc1e0bb74e724d6b0
Kaan Eraslan <kaaneraslan@gmail.com>
2020-01-31 03:01:06 +0100

    separator equality ops implemented

--- src/dtype/separator.nim
+++ src/dtype/separator.nim
@@ -8,14 +8,35 @@ type
                    # default value
         s* = "-"   # sensible value
 
+proc `==`(s1, s2: US): bool =
+    return s1.d == s2.d
+
+proc `!=`(s1, s2: US): bool =
+    return not(s1 == s2)
+
 type
     RS* = object
         d* = "" # record separator: U+001E
                    # default value
         s* = "_"   # sensible value
 
+proc `==`(s1, s2: RS): bool =
+    return s1.d == s2.d
+
+proc `!=`(s1, s2: RS): bool =
+    return not(s1 == s2)
+
+
 type
     GS* = object
         d* = "" # group separator: U+001D
                    # default value
         s* = ":"   # sensible value
+
+proc `==`(s1, s2: GS): bool =
+    return s1.d == s2.d
+
+proc `!=`(s1, s2: GS): bool =
+    return not(s1 == s2)
+
+
